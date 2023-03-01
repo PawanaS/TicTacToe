06cdf6a2c922f85cda3cb128aa272f24aeb22f5c
Paula <panna.na@gmail.com>
2023-03-01 16:14:53 -0500

    commiting

---
+++ README.md
@@ -0,0 +1,27 @@
+# Tic Tac Toe Game
+
+This is a simple two-player Tic Tac Toe game built using HTML, CSS, and JavaScript. The game features a responsive.
+
+## How to Play
+
+To play the game, simply click on the cell where you want to place your marker (`X` or `O`). The first player to get three in a row (horizontally, vertically, or diagonally) wins the game. If all cells are filled and there is no winner, the game is considered a draw.
+To start a new game, click the "Restart" button. Each Turn of "X" and "O" goes toggling one after another when being clicked until all the cells are used. At the end winner is declared  or game is draw as per the cell array meets different combinations of arrays.
+
+## Features
+
+- Two-player game (either against the computer or against another player)
+- Keeps track of each player's score
+- Easy-to-use click restart to start a new game
+-
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- DOM
+
+## Dependencies 
+ex.windows 10
+
+
+
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 // selecting all the element in game board cell and make array cellElements
 const cellElements = document.querySelectorAll('.game-board .cell')
-const player1 = document.querySelector(".players .players1")
-const player2 = document.querySelector(".players .players2")
+const player1=document.querySelector(".players .player1");
+const player2=document.querySelector(".players .player2");
+
+
+const playerO = 'O'
+const playerX = "X"
+let toggleTurn= true;
 const WINNING_CONDITIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -14,4 +19,63 @@ const WINNING_CONDITIONS = [
     [2, 4, 6],
 
 ]
+//console.log(cellElements)
+cellElements.forEach(cell => {
+    // console.log(cell)
+    cell.onclick = () => {
+        let currentPlayer = toggleTurn ? playerO : playerX;//toggles turns between O and X after each click in the cells.
+        //console.log(cell)
+       
+        cell.classList.add("disabled")//disabled class is applied in each cell after each one is clicked.
+        //cell.innerHTML = currentPlayer;
+       
 
+        addInCell(cell, currentPlayer);
+        swapPlayer();
+        if (winnerCheck (currentPlayer)){// today march 1
+            console.log(currentPlayer,"is a WINNER!")
+        }
+        else if (isDraw){
+                console.log("Draw the game!")
+            }else{
+                
+            }
+
+        }
+    })
+
+
+function swapPlayer(){
+    toggleTurn = !toggleTurn;//working till here
+if(toggleTurn){// this if else is to hit the turn-bottom one after another
+    player1.classList.add("active");
+    player2.classList.remove("active");
+}else{
+    player2.classList.add("active");
+    player1.classList.remove("active");//working till here
+}
+
+}   
+function isDraw(){
+    return [...cellElements].every(cell=>{
+      return cell.classList.contains(playerX)||cell.classList.contains(playerO)//work from here
+
+})
+
+}
+
+function addInCell(cell, currentPlayer){
+    cell.innerHTML = currentPlayer;
+    cell.classList.add(currentPlayer);//add current players in the class currentList.
+
+}
+
+function winnerCheck(currentPlayer){
+return WINNING_CONDITIONS.some(condition=>{
+    //console.log(condition)
+    return condition.every(index=>{
+    return cellElements[index].classList.contains(currentPlayer)
+    // console.log (cellElements[index].classList.contains(currentPlayer))
+
+    })
+})}
